f292be8ad27ab27b06c469f487cdf98c01b5a881
Gabriela Gonzalez <gabriela>
2024-07-26 12:57:58 -0400

    Initial Commit

---
+++ Final Project KWK/Final_Project_KWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_KWKApp.swift
+//  Final Project KWK
+//
+//  Created by Gabriela Gonzalez on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_KWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
